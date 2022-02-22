d618b18979bb5ad68fa1c364490206da058611d7
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-02-22 20:03:34 +0900

    Update pswrd.js

--- pswrd.js
+++ pswrd.js
@@ -1,5 +1,8 @@
 function SuBmIt(){
   let Abc = document.getelementbyID('PlgpQ')
   let vlu = Abc.value
-  if(vlu === secret.SECRET_PSWRD)
+  if(vlu === secret.SECRET_PSWRD){
+  alert('√');
+  location.replace('./ADTs');
+}
 }
